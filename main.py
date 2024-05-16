21c15753bd064812a1a50afb976d281b85720f2a
Vladislav Korecký <vladislav.ml@korecky.org>
2024-05-16 16:37:15 +0200

    V2

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from time import time
+from os.path import isfile
+
 
 hunger = 0
+food = 0
 start_time = time()
 
-f = open("save.txt", "w")
-f.write(str(start_time))
-f.close()
+if isfile("save.txt"):
+    f = open("save.txt", "r")
+    content = f.read().split("\n")
+    start_time = float(content[0])
+    food = int(content[1])
+
+
+def save():
+    f = open("save.txt", "w")
+    content = f"{start_time}\n{food}"
+    f.write(content)
+    f.close()
+
+
+save()
+
 
 while True:
-    input()
+    prikaz = input()
+
+    if prikaz == "brambora":
+        food = food + 5
+        save()
+
     end_time = time()
 
-    hunger = (end_time - start_time) / 2
+    hunger = (end_time - start_time) / 2 - food
 
     print(int(hunger))
